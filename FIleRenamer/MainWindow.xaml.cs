1f77b5d9712ab9015eb11056881b392682f26a16
Kuba <kubalisowski94@gmail.com>
2020-08-08 15:57:31 +0200

    Add project files.

---
+++ FIleRenamer/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace FIleRenamer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "RenameTheFile!";
+        }
+
+        private void gobtn_Click(object sender, RoutedEventArgs e)
+        {
+            Rename DoRename = new Rename(pathtxtbx.Text, subdirschkbx.IsEnabled);
+
+            //Match checkpattern = Regex.Match(pathtxtbx.Text, stringtxtbx.Text);
+            string pattern = @".*" + stringtxtbx.Text + @".*";
+            Regex regex = new Regex(pattern);
+            
+            foreach (string file in DoRename.FilesFromDir)
+            {
+                foreach (Match match in regex.Matches(file))
+                {
+                    // From System.IO.
+                    File.Move(file, file.Replace(stringtxtbx.Text, @""));
+                    
+                }
+            }
+           
+        }
+
+        private void subdirschkbx_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FIleRenamer/Rename.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace FIleRenamer
+{
+    class Rename
+    {
+        public string[] FilesFromDir = new string[] { "" };
+        public Rename(string path, bool searchoption)
+        {
+            SearchOption SubDirs(bool includeSubDirs)
+            {
+                if (searchoption)
+                    return SearchOption.AllDirectories;
+                else
+                    return SearchOption.AllDirectories;
+            }
+
+            try
+            {
+                FilesFromDir = Directory.GetFiles(path, "*.*", SubDirs(searchoption));
+            }
+            catch
+            {
+                MessageBox.Show("Please check provided path!");
+            }
+        }
+    }
+
+}
+
